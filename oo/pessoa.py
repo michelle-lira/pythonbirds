16dce2d6b04375a22a8d744c1064d31a5cb5b89d
Michelle Lira <mchll.lira@gmail.com>
2021-04-19 16:07:22 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
-    def cumprimentar(self):
+    def __init__(self, nome=None, idade=25):
+        self.idade = idade
+        self.nome = nome  # sel.nome->atributo do objeto | nome->parâmetro
+
+    def cumprimentar(self):  # atributo da classe
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Ben')
     print(Pessoa.cumprimentar(p))  # passando o parâmetro p
     print(id(p))
     print(p.cumprimentar())  # deste modo também estou passando o parâmetro p
+    print(p.nome)  # acessando atributo através do próprio objeto
+    p.nome = 'Luan'  # alterando o valor do atributo
+    print(p.nome)
+    print(p.idade)
